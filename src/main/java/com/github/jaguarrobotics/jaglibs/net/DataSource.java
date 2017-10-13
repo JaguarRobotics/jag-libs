7551b99d586236d5cad62341275df45340e595f9
Zach Deibert <zachdeibert@gmail.com>
2017-10-13 11:24:49 -0500

    Switched to ByteBuffers

--- src/main/java/com/github/jaguarrobotics/jaglibs/net/DataSource.java
+++ src/main/java/com/github/jaguarrobotics/jaglibs/net/DataSource.java
@@ -14,7 +14,7 @@ public class DataSource {
     private final NetClient                     client;
     private final String                        topic;
     private final Set<Consumer<RealCoordinate>> callbacks;
-    private final Set<Consumer<byte[]>>         rawCallbacks;
+    private final Set<Consumer<ByteBuffer>>     rawCallbacks;
 
     public void on(Consumer<RealCoordinate> callback) {
         if (callback == null) {
@@ -24,7 +24,7 @@ public class DataSource {
         client.strongReferences.add(this);
     }
 
-    public void onRaw(Consumer<byte[]> callback) {
+    public void onRaw(Consumer<ByteBuffer> callback) {
         if (callback == null) {
             throw new IllegalArgumentException("Callback cannot be null");
         }
@@ -42,7 +42,7 @@ public class DataSource {
         }
     }
 
-    public void removeRawListener(Consumer<byte[]> callback) {
+    public void removeRawListener(Consumer<ByteBuffer> callback) {
         if (callback == null) {
             throw new IllegalArgumentException("Callback cannot be null");
         }
@@ -64,11 +64,11 @@ public class DataSource {
         on(proxy.value);
     }
 
-    public void onceRaw(Consumer<byte[]> callback) {
+    public void onceRaw(Consumer<ByteBuffer> callback) {
         if (callback == null) {
             throw new IllegalArgumentException("Callback cannot be null");
         }
-        Pointer<Consumer<byte[]>> proxy = new Pointer<Consumer<byte[]>>();
+        Pointer<Consumer<ByteBuffer>> proxy = new Pointer<Consumer<ByteBuffer>>();
         proxy.value = c-> {
             removeRawListener(proxy.value);
             callback.accept(c);
@@ -76,7 +76,7 @@ public class DataSource {
         onRaw(proxy.value);
     }
 
-    public void emit(byte[] value, int qos, boolean retain) throws IOException {
+    public void emit(ByteBuffer value, int qos, boolean retain) throws IOException {
         if (value == null) {
             throw new IllegalArgumentException("value cannot be null");
         }
@@ -84,13 +84,13 @@ public class DataSource {
             throw new IllegalArgumentException("Invalid QoS value");
         }
         try {
-            client.client.getTopic(topic).publish(value, qos, retain);
+            client.client.getTopic(topic).publish(value.array(), qos, retain);
         } catch (MqttException ex) {
             throw new IOException(ex);
         }
     }
 
-    public void emit(byte[] value) throws IOException {
+    public void emit(ByteBuffer value) throws IOException {
         emit(value, 2, false);
     }
 
@@ -98,14 +98,21 @@ public class DataSource {
         if (value == null) {
             throw new IllegalArgumentException("value cannot be null");
         }
-        emit(value.serialize().array());
+        emit(value.serialize());
     }
 
     @SuppressWarnings("unchecked")
     void messageArrived(MqttMessage message) throws Exception {
-        RealCoordinate c = RealCoordinate.deserialize(ByteBuffer.wrap(message.getPayload()));
-        for (Consumer<RealCoordinate> callback : callbacks.toArray(new Consumer[0])) {
-            callback.accept(c);
+        if (!rawCallbacks.isEmpty()) {
+            for (Consumer<ByteBuffer> callback : rawCallbacks.toArray(new Consumer[0])) {
+                callback.accept(ByteBuffer.wrap(message.getPayload()));
+            }
+        }
+        if (!callbacks.isEmpty()) {
+            RealCoordinate c = RealCoordinate.deserialize(ByteBuffer.wrap(message.getPayload()));
+            for (Consumer<RealCoordinate> callback : callbacks.toArray(new Consumer[0])) {
+                callback.accept(c);
+            }
         }
     }
 
@@ -118,7 +125,7 @@ public class DataSource {
         this.client = client;
         this.topic = topic;
         callbacks = new HashSet<Consumer<RealCoordinate>>();
-        rawCallbacks = new HashSet<Consumer<byte[]>>();
+        rawCallbacks = new HashSet<Consumer<ByteBuffer>>();
         try {
             client.client.subscribe(topic, 2);
         } catch (MqttException ex) {
